ba8f37436fbe8becb9d683fca98d3c755b40903f
NriotHrreion <nriot233@outlook.com>
2022-08-23 18:16:46 +0800

    perf: Add layout adaptations

--- src/pages/homepage/Header.tsx
+++ src/pages/homepage/Header.tsx
@@ -7,13 +7,15 @@ const Header: React.FC = () => {
     return (
         <header className="header-container">
             <div className="banner">
-                <div className="title">
-                    <h1>HYCDGX</h1>
-                    <p>一个小型Minecraft生存服</p>
-                </div>
-                <div className="quick-start-bar">
-                    <ButtonLink to="/quick-start">快速开始</ButtonLink>
-                    <ButtonLink to={qq} newtab>加入QQ群</ButtonLink>
+                <div className="header-content">
+                    <div className="title">
+                        <h1>HYCDGX</h1>
+                        <p>一个小型Minecraft生存服</p>
+                    </div>
+                    <div className="quick-start-bar">
+                        <ButtonLink to="/quick-start">快速开始</ButtonLink>
+                        <ButtonLink to={qq} newtab>加入QQ群</ButtonLink>
+                    </div>
                 </div>
             </div>
         </header>
